57df70cb71c057de3b906f066500bcef87b289e9
Yash <kudveyash@gmail.com>
2024-07-10 16:17:46 +0530

    Symbol declration

--- Basics/Object.js
+++ Basics/Object.js
@@ -1,11 +1,16 @@
+const mySym = Symbol()
 const user = {
     name: 'user2',
     age:15,
     location:"Mumbai",
+    [mySym]:"key5"
 
 }
 
 // console.log(user.name)
 // console.log(user.age)
 
-console.table([user.name, user.age,user.location])
+// console.table([user.name, user.age,user.location])
+
+// console.log(mySym)
+console.log(user[mySym])
