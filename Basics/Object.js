eaf9d90359b3e5430fa75d7c84b006ead2d0fb69
Yash <kudveyash@gmail.com>
2024-07-10 16:12:39 +0530

    changes

---
+++ Basics/Object.js
@@ -0,0 +1,11 @@
+const user = {
+    name: 'user2',
+    age:15,
+    location:"Mumbai",
+
+}
+
+// console.log(user.name)
+// console.log(user.age)
+
+console.table([user.name, user.age,user.location])
