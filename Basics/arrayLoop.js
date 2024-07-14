c7ab1406e28424c8189c08272fe72d570b0ca3aa
Yash <kudveyash@gmail.com>
2024-07-14 17:35:13 +0530

    changes

---
+++ Basics/arrayLoop.js
@@ -0,0 +1,12 @@
+// Array can be a list of element "or" a list of objects
+
+// ["","",""]
+// [{},{},{}]
+
+// for of loop
+
+let arr = [1,2,3,4,5,6]
+for (const num of arr) {
+    console.log(num)
+}
+
