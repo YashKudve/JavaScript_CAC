03a0cfe76e87bea665360e8e0ba4d89406271830
Yash <kudveyash@gmail.com>
2024-07-14 17:13:19 +0530

    changes

--- Basics/loops.js
+++ Basics/loops.js
@@ -1,10 +1,34 @@
-for(i=1;i<=10;i++){
-    console.log(i)
+// for(i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+// for(i=1;i<=10;i++){
+//     for(j=1;j<=10;j++){
+//         console.log(i + "*" + j + "=" + i*j)
+//     }
+//     console.log('**************************************')
+// }
+
+// break statement
+
+for (let index = 0; index < 11; index++) {
+    console.log(`The number is ${index}`)
+
+    if(index == 5){
+        console.log("Number 5")
+        break;
+    }
+    
 }
 
-for(i=1;i<=10;i++){
-    for(j=1;j<=10;j++){
-        console.log(i + "*" + j + "=" + i*j)
+// continue statement
+
+for (let index = 0; index < 11; index++) {
+    console.log(`The number is ${index}`)
+
+    if(index == 5){
+        console.log("Number 5")
+        continue;
     }
-    console.log('**************************************')
+    
 }
