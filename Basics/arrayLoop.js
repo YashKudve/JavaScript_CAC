e1020d7cd7b70d801bd30ddacbac02f37cf6b7d3
Yash <kudveyash@gmail.com>
2024-07-14 17:41:09 +0530

    changes

--- Basics/arrayLoop.js
+++ Basics/arrayLoop.js
@@ -5,8 +5,24 @@
 
 // for of loop
 
-let arr = [1,2,3,4,5,6]
-for (const num of arr) {
-    console.log(num)
-}
+// let arr = [1,2,3,4,5,6]
+// for (const num of arr) {
+//     console.log(num)
+// }
 
+// let greeting = "Hello YASH"
+// for (const greet of greeting) {
+//     // console.log(greet)
+
+//     if (greet==" ") {
+//         break
+//         continue
+//     }
+//     console.log(greet)
+// }
+
+// let country = ["India","Pakistan","Scotland","italy","Romania"]
+
+// for (const place of country) {
+//    console.log(place) 
+// }
