a879715e1924ea52428619fa193677cf0086866b
Yash <kudveyash@gmail.com>
2024-07-14 17:26:52 +0530

    changes

--- Basics/loops.js
+++ Basics/loops.js
@@ -11,24 +11,47 @@
 
 // break statement
 
-for (let index = 0; index < 11; index++) {
-    console.log(`The number is ${index}`)
+// for (let index = 0; index < 11; index++) {
+//     console.log(`The number is ${index}`)
 
-    if(index == 5){
-        console.log("Number 5")
-        break;
-    }
+//     if(index == 5){
+//         console.log("Number 5")
+//         break;
+//     }
     
-}
+// }
 
 // continue statement
 
-for (let index = 0; index < 11; index++) {
-    console.log(`The number is ${index}`)
+// for (let index = 0; index < 11; index++) {
+//     console.log(`The number is ${index}`)
 
-    if(index == 5){
-        console.log("Number 5")
-        continue;
-    }
+//     if(index == 5){
+//         console.log("Number 5")
+//         continue;
+//     }
     
-}
+// }
+
+
+// while loop
+
+// let index=1
+
+// while (index<=5) {
+//     console.log(index)
+//     index++
+// }
+
+// let a = 1
+// do {
+//     console.log(a)
+//     a=a+2
+// } while (a<=10);
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+   score=score+2;
+} while (score<=10);
