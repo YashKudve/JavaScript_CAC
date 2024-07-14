856d1574dff3a9247fddde9108b4fd0da2638830
Yash <kudveyash@gmail.com>
2024-07-14 18:36:50 +0530

    for-in loop

--- Basics/objectLoop.js
+++ Basics/objectLoop.js
@@ -4,7 +4,17 @@ const myObject = {
     game3:'Geo-guesser'
 }
 
+// for-of loop (this cannot be used for objects)
 // for (const game of myObject) {
 //     console.log(game)
 // }
 
+
+// for-in loop(this is used for objects)
+
+for (const key in myObject) {
+    // console.log(key) --> this prints key
+    // console.log(myObject[key]) --> this prints value
+
+    console.log(`'${key}' shortcut is for '${myObject[key]}'`)
+}
