2396e5656aca578937829ca539154ca9b08d8135
Yash <kudveyash@gmail.com>
2024-07-15 16:46:47 +0530

    changes

--- Basics/objectInArray.js
+++ Basics/objectInArray.js
@@ -1,36 +1,44 @@
-const coding = [
-    {
-        languageName:"JavaScript",
-        languageFileName:'js',
-        colour:{
-            red:'red'
-        }
-    },
-    {
-        languageName:"Java",
-        languageFileName:'java',
-        colour:{
-            red:'red'
-        }
-    },
-    {
-        languageName:"Python",
-        languageFileName:'py',
-        colour:{
-            red:'orange'
-        }
-    },
-]
+// const coding = [
+//     {
+//         languageName:"JavaScript",
+//         languageFileName:'js',
+//         colour:{
+//             red:'red'
+//         }
+//     },
+//     {
+//         languageName:"Java",
+//         languageFileName:'java',
+//         colour:{
+//             red:'red'
+//         }
+//     },
+//     {
+//         languageName:"Python",
+//         languageFileName:'py',
+//         colour:{
+//             red:'orange'
+//         }
+//     },
+// ]
 
 // coding.forEach((item)=>(
 //     // console.log(item.languageFileName)
 //     // console.log(item.colour.red)
 // ))
 
-const myNum = [1,2,3,4]
-const values = myNum.forEach((item)=>(
-    console.log('')
+// const myNum = [1,2,3,4]
+// const values = myNum.forEach((item)=>(
+//     console.log('')
+// ))
+
+// console.log(values)
+//*********IMPORTANT: for-each loop does not return any value***********
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const numbers = myNums.filter((number)=>(
+    number>4
 ))
 
-console.log(values)
-//*********IMPORTANT: for-each loop does not return any value***********
+console.log(numbers)
