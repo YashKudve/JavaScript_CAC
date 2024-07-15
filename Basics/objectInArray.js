48e9ddd54fe101cb191d3eb15b876c6854894a31
Yash <kudveyash@gmail.com>
2024-07-15 16:56:58 +0530

    changes

--- Basics/objectInArray.js
+++ Basics/objectInArray.js
@@ -35,10 +35,64 @@
 // console.log(values)
 //*********IMPORTANT: for-each loop does not return any value***********
 
-const myNums = [1,2,3,4,5,6,7,8,9,10]
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
 
-const numbers = myNums.filter((number)=>(
-    number>4
-))
+// const numbers = myNums.filter((number)=>(
+//     number>4
+// ))
 
-console.log(numbers)
+// console.log(numbers)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const books = [
+    {
+        title:"Book1",
+        genre:"Sci-Fi",
+        publish: 2000,
+        edition: 2004
+    },
+    {
+        title:"Book2",
+        genre:"Story",
+        publish: 2010,
+        edition: 2015
+    },
+    {
+        title:"Book3",
+        genre:"Drama",
+        publish: 2010,
+        edition: 2019
+    },
+    {
+        title:"Book4",
+        genre:"Comic",
+        publish: 1995,
+        edition: 2001
+    },
+    {
+        title:"Book5",
+        genre:"History",
+        publish: 1985,
+        edition: 2003
+    },
+    {
+        title:"Book6",
+        genre:"Novels",
+        publish: 2020,
+        edition: 2023
+    },
+    {
+        title:"Book7",
+        genre:"Non-fiction",
+        publish: 2006,
+        edition: 2016
+    },
+    {
+        title:"Book8",
+        genre:"Geography",
+        publish: 2009,
+        edition: 2019
+    },
+]
