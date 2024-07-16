972db58f8076c4b9f8da0dfec72e2db1bc262f93
Yash <kudveyash@gmail.com>
2024-07-16 16:52:16 +0530

    chaning method

--- Basics/mapMethod.js
+++ Basics/mapMethod.js
@@ -1,7 +1,12 @@
 let myNumbers = [1,2,3,4,5,6,7,8,9,10]
 
-const newNums = myNumbers.map((num)=>(
-    num+10
-))
+// const newNums = myNumbers.map((num)=>(
+//     num+10
+// ))
 
+
+const newNums = myNumbers.map((num)=>num+1).map((num)=>num*10).filter((num)=>num>=40)
 console.log(newNums)
+
+// [2,3,4,5,6,7,8,9....]
+// [20,30,40,50,...]
