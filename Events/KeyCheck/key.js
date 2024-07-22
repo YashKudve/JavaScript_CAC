9f280bbaa88586ed881214663aeaec3c73a92610
Yash <kudveyash@gmail.com>
2024-07-22 17:05:00 +0530

    key checker

---
+++ Events/KeyCheck/key.js
@@ -0,0 +1,23 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (e)=>{
+    insert.innerHTML = `
+    <div class='color'>
+        <table style = 'border: 1px solid white;
+  border-collapse: collapse;'>
+            <tr style = 'border: 1px solid white;
+  border-collapse: collapse;'>
+                <th>Key</th>
+                <th>KeyCode</th>
+                <th>Code</th>
+            </tr>
+            <tr >
+                <td>${e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+    
+        </table>
+</div>
+    `
+})
