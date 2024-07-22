e08f7121a41f6a9bd80ca9395ad8f944b5fa9063
Yash <kudveyash@gmail.com>
2024-07-22 15:44:25 +0530

    update

--- Events/colorChange/home.js
+++ Events/colorChange/home.js
@@ -1,9 +1,18 @@
 const randomColor = function(){
     const hex = '0123456789ABCDEF'
-    const color = '#'
+    let color = '#'
 
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)]
         
     }
+    return color
 }
+
+// console.log(randomColor())
+
+const startColorChange = function(){}
+const stopColorChange = function(){}
+
+document.querySelector('#start').addEventListener('click', startColorChange)
+document.querySelector('#stop').addEventListener('click', stopColorChange)
