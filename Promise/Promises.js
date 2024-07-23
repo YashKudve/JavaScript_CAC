c3405db546b953fb8f9db41495e4334be02a64d7
Yash <kudveyash@gmail.com>
2024-07-23 17:02:12 +0530

    fetch method

--- Promise/Promises.js
+++ Promise/Promises.js
@@ -58,23 +58,39 @@
 
 // *********** Promise 5 ******************
 
-const promiseFive = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error = false;
-        if (!error) {
-            resolve({username:'JavaScript', password:'1234'})
-        } else {
-            reject("Something went wrong")
-        }
-    },1000)
-})
+// const promiseFive = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = false;
+//         if (!error) {
+//             resolve({username:'JavaScript', password:'1234'})
+//         } else {
+//             reject("Something went wrong")
+//         }
+//     },1000)
+// })
 
-async function consumePromiseFive(){
+// async function consumePromiseFive(){
+//     try {
+//         const response = await promiseFive
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// consumePromiseFive()
+
+// ********************************************
+
+async function getAllUsers(){
     try {
-        const response = await promiseFive
-        console.log(response);
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        // console.log(response)
+        const data = await response.json()
+        console.log(data)
+
     } catch (error) {
         console.log(error);
     }
 }
-consumePromiseFive()
+
+getAllUsers()
