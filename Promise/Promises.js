f7a88238970e7fd87991b53a70c5c2fdb82bfdd1
Yash <kudveyash@gmail.com>
2024-07-23 15:38:07 +0530

    create and consume promises

--- Promise/Promises.js
+++ Promise/Promises.js
@@ -1,9 +1,19 @@
-const promiseOne = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task complete");
-    },1000)
-})
+// const promiseOne = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task complete");
+//         resolve() //due to this .then executes
+//     },1000)
+// })
 
-promiseOne.then(function(){
-    console.log('Promise consumed');
+// promiseOne.then(function(){
+//     console.log('Promise consumed');
+// })
+
+// *************Another method for promise***********
+
+new Promise(function(resolve,reject){
+    console.log("Async Task 2");
+    resolve()
+}).then(function(){
+    console.log('Async2 resolved');
 })
