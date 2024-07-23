1a463f3883e8d08c6cdcf7d3d855daf5bb7d0683
Yash <kudveyash@gmail.com>
2024-07-23 16:24:59 +0530

    .then .catch .finally and chaining promises

--- Promise/Promises.js
+++ Promise/Promises.js
@@ -11,11 +11,47 @@
 
 // *************Another method for promise***********
 
-new Promise(function(resolve,reject){
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//     console.log("Async Task 2");
+//     resolve()
+//     },1000)
+// }).then(function(){
+//     console.log('Async2 resolved');
+// })
+
+
+// ************ Promise 3 ***********
+// const promiseThree = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve({username:"Yash", email:'yash@google.com'})
+//     },1000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
+
+// **************** Promise 4 ****************
+
+const promiseFour = new Promise(function(resolve,reject){
+
     setTimeout(function(){
-    console.log("Async Task 2");
-    resolve()
+        let error = true;
+
+        if (!error) {
+            resolve({username:'ABC', password:"123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
     },1000)
-}).then(function(){
-    console.log('Async2 resolved');
 })
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username;
+}).then((username)=>{
+    console.log(username);
+}).catch((error)=>{
+    console.log(error)
+}).finally(()=>console.log('The promise is either resolved or rejected...'))
