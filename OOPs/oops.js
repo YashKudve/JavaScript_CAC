bc43692604a0d8ca8c0c4580fa8ca6164a8e6d5d
Yash <kudveyash@gmail.com>
2024-07-25 15:38:28 +0530

    new keyword for different instances

--- OOPs/oops.js
+++ OOPs/oops.js
@@ -1,13 +1,32 @@
-const user = {
-    username: 'Yash',
-    loginCount: 8,
-    signedIn: true,
+// const user = {
+//     username: 'Yash',
+//     loginCount: 8,
+//     signedIn: true,
 
-    getUserDetails: function () {
-        // console.log(`Username: ${this.username}`);
-        // console.log(`Logged in:  ${this.loginCount}`);
-        // console.log(this);
-    }
+//     getUserDetails: function () {
+//         console.log(`Username: ${this.username}`);
+//         console.log(`Logged in:  ${this.loginCount}`);
+//         console.log(this);
+//     }
+// }
+
+// console.log(user.getUserDetails());
+
+function User(username,loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this;
 }
 
-console.log(user.getUserDetails());
+// const userOne = User("Yash", 11 , true)
+// const userTwo = User("ABC", 45 , true)
+
+// console.log(userOne);
+
+const userOne =new User("Yash", 11 , true)
+const userTwo = new User("ABC", 45 , true)
+
+console.log(userOne);
+console.log(userTwo);
