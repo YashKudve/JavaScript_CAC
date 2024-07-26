55bcd529f3297f6c132989299ff0e673ba161eee
Yash <kudveyash@gmail.com>
2024-07-26 17:07:11 +0530

    changes

--- OOPs/object.js
+++ OOPs/object.js
@@ -1,9 +1,27 @@
-function multBy5(num){
-    return num*5
+// function multBy5(num){
+//     return num*5
+// }
+
+// multBy5.power = 2;
+
+// console.log(multBy5(5));
+// console.log(multBy5.power);
+// console.log(multBy5.prototype);
+
+function createUser(username, score){
+    this.username = username;
+    this.score = score;
 }
 
-multBy5.power = 2;
+createUser.prototype.increment = function(){
+    this.score++;
+}
 
-console.log(multBy5(5));
-console.log(multBy5.power);
-console.log(multBy5.prototype);
+createUser.prototype.printMe = function(){
+    console.log(`score is ${this.score}`);
+}
+
+const yash = createUser('Yash', 20)
+const Ameya = createUser('Ameya', 40)
+
+console.log(yash);
