c4da29710feac95ab7f0b01e0ca30e0308eb2306
Yash <kudveyash@gmail.com>
2024-07-29 15:27:55 +0530

    call method and this keyword

--- OOPs/Call.js
+++ OOPs/Call.js
@@ -1,13 +1,14 @@
-function SetUsername(){
+function SetUsername(username){
     this.username = username;
 }
 
 function createUser(username, email, password){
 
-    SetUsername(username)
+    // SetUsername(username)
+    SetUsername.call(this,username)
     this.email = email;
     this.password = password
 }
 
-const user = new createUser('Yash','yash@gmail.com',123456)
+const user = new createUser('Yash Kudve','yash@gmail.com',123456)
 console.log(user);
