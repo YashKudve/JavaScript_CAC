c5ac6199aa417bda1633afcede3bef08482ddbdd
Yash <kudveyash@gmail.com>
2024-07-30 17:49:40 +0530

    descriptor properties of objects

---
+++ Classes/mathpi.js
@@ -0,0 +1,13 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+
+console.log(descriptor)
+
+// const myObj = Object()
+
+const chai = {
+    name: 'Green tea',
+    price: 200,
+    isAvailable: true
+}
+
+console.log(chai);
