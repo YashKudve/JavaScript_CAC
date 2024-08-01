396e840fa3ce0447169ca91447fbe2590278676d
Yash <kudveyash@gmail.com>
2024-08-01 16:40:42 +0530

    Lexical Scoping

---
+++ Closures/lexicalScoping.js
@@ -0,0 +1,16 @@
+function init(){
+    let name = 'Yash';
+
+    function displayName(){
+        console.log(name);
+        // let secret = 'my secret'
+    }
+
+    // function print(){
+    //     console.log(secret);
+    // }
+    // print()
+    displayName()
+}
+
+init()
