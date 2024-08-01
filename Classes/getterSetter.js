c9ca575be9f3ddac864fa270a3ac588006780a35
Yash <kudveyash@gmail.com>
2024-08-01 12:09:28 +0530

    getter setter

--- Classes/getterSetter.js
+++ Classes/getterSetter.js
@@ -3,7 +3,25 @@ class User{
         this.email = email;
         this.password = password
     }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email= value
+    }
+
+
+    get password(){
+        // return this._password.toUpperCase()
+        return `${this._password}abc123`.toUpperCase()
+    }
+
+    set password(value){
+        this._password = value
+    }
 }
 
-const yash = new User('yash@g.com', '1234')
+const yash = new User('yash@g.com', 'yAShg')
 console.log(yash.password);
